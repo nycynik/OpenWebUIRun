bd15bbb751ae34f180963ec69b2daf8c2c2cba76
Mike Lynch <nycynik@gmail.com>
2024-07-09 08:49:15 -0700

    adding gpu and non-gpu split

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-docker run -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+
+python3 -m webbrowser http://localhost:3000/workspace/models
+python3 -m webbrowser http://localhost:3000/admin/settings
+
+
